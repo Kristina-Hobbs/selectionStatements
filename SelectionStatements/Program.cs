d2584caa6b7569ba15729a7c097f36ee7e7a9325
Kristina Hobbs <kjcagle86@gmail.com>
2022-05-11 14:48:45 -0400

    initial commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the number guessing game!");
+
+
+            var favNumber = 17;
+
+            Console.WriteLine("Can you guess my favorite number?");
+            string guess = Console.ReadLine();
+            int userGuess = int.Parse(guess);
+
+            bool condition = true;
+
+            if(userGuess == favNumber)
+            {
+                Console.WriteLine("That is correct!");
+            }
+            else
+            {
+                Console.WriteLine("Thats not it, try again!");
+
+            }
+        }
+    }
+}
